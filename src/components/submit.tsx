880b46e44cd5f425eab57cff91da20332d204d09
Sunghyun Cho <hey@cho.sh>
2024-09-16 16:52:04 -0700

    refactor: Update SubmitButton component to accept a variant prop

--- src/components/submit.tsx
+++ src/components/submit.tsx
@@ -1,15 +1,20 @@
 'use client'
 
 import { useFormStatus } from 'react-dom'
-import { Button } from '@/components/ui/button'
+import { Button, ButtonProps } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 
-const SubmitButton = (props: { children: React.ReactNode; className?: string }) => {
+const SubmitButton = (props: { children: React.ReactNode; className?: string; variant?: ButtonProps['variant'] }) => {
   const { pending } = useFormStatus()
 
   return (
-    <Button className={cn('flex items-center justify-center gap-1', props.className)} disabled={pending} type="submit">
+    <Button
+      className={cn('flex items-center justify-center gap-2', props.className)}
+      disabled={pending}
+      type="submit"
+      variant={props.variant ?? 'default'}
+    >
       {pending ? <Loader2 className="size-4 animate-spin" /> : props.children}
     </Button>
   )
